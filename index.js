7d798de6b9dea9a672d903c00fa9fb2794afb001
Lucas <lucas.faugeron56320@gmail.com>
2023-05-30 12:26:23 +0200

    adding index.js for default exporting

---
+++ index.js
@@ -0,0 +1,5 @@
+import DayNightToggle from "./DayNightToggle";
+import {useThemeContext} from "./ThemeContextProvider";
+import {ThemeContextProvider} from "./ThemeContextProvider";
+
+export default {DayNightToggle, useThemeContext, ThemeContextProvider}
